18e2f842a8aef25b54ea6dc4f11333e278fb495d
Cauã Clemente <cauaclemente07@gmail.com>
2024-03-26 16:17:20 -0300

    Layout do dashboard

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,7 +1,10 @@
 import Head from "next/head"
+
 import { canSSRAuth } from "../../utils/canSSRAuth"
+import styles from "./styles.module.scss"
 import Header from "@/components/header"
 
+import { FiRefreshCcw } from "react-icons/fi"
 
 export default function Dashboard(){
   return(
@@ -11,6 +14,23 @@ export default function Dashboard(){
     </Head>
       <div>
         <Header />
+        <main className={styles.container}>
+          <div className={styles.containerHeader}>
+            <h1>Ultimos pedidos</h1>
+            <button>
+              <FiRefreshCcw size={25} color="#3fffa3" />
+            </button>
+          </div>
+            <article className={styles.listOrders}>
+              <section className={styles.orderItem}>
+                <button>
+                  <div className={styles.tag}></div>
+                  <span> Mesa 30</span>
+                </button>
+              </section>
+            </article>
+        </main>
+
       </div>
     </>
   )
